66f86b99b675727ad862924423ce359fdeaf4a5a
Joshua D'Souza <joshua@fastapps.co>
2019-02-19 17:02:18 -0500

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jacinto d'souza\documents\waterloo\management engineering\3a\msci 436 - decision support systems\project\mpg_predictor\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
